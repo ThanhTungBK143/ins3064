760db1e5bd75e34d6bdbde5cc256bf3ca121fad5
ThanhTungBK143 <mathanhtungbk2004@gmail.com>
2025-09-23 17:54:21 +0700

    Create AllOperators.php

---
+++ AllOperators.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        //http://ins3064.test/AllOperators.php?x=10&y=11
+        $x = $_GET["x"];
+        $y = $_GET["y"];
+        // Arithmetric operators
+        echo "x: " . $x . "<br/>";
+        echo "y: " . $y . "<br/>";
+        echo "x/y: " . ($x / $y) . "<br/>";
+        echo "x%y: " . ($x * $y) . "<br/>";
+        echo "x++: " . ($x++) . "<br/>";
+        echo "++y: " . (++$y) . "<br/>";
+        // Comparison operators
+        echo "x == y: " . ($x == $y) . "<br/>";
+        echo "x != y: " . ($x != $y) . "<br/>";
+        echo "x < y: " . ($x < $y) . "<br/>";
+        echo "x > y: " . ($x > $y) . "<br/>";
+        echo "x <= y: " . ($x <= $y) . "<br/>";
+        echo "x >= y: " . ($x >= $y) . "<br/>";
+        ?>
+    </body>
+</html>
