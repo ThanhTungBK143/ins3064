e1ddd8c23540f84b1e0cc4e5d94f9478d952e7c2
ThanhTungBK143 <mathanhtungbk2004@gmail.com>
2025-09-23 17:33:33 +0700

    Update ComparisonOperators.php

--- ComparisonOperators.php
+++ ComparisonOperators.php
@@ -8,12 +8,12 @@
         <?php
         $x = 10;
         $y = 11;
-        echo "x == y" . ($x == $y) . "<br/>";
-        echo "x != y" . ($x != $y) . "<br/>";
-        echo "x < y" . ($x < $y) . "<br/>";
-        echo "x > y" . ($x > $y) . "<br/>";
-        echo "x <= y" . ($x <= $y) . "<br/>";
-        echo "x >= y" . ($x >= $y) . "<br/>";
+        echo "x == y: " . ($x == $y) . "<br/>";
+        echo "x != y: " . ($x != $y) . "<br/>";
+        echo "x < y: " . ($x < $y) . "<br/>";
+        echo "x > y: " . ($x > $y) . "<br/>";
+        echo "x <= y: " . ($x <= $y) . "<br/>";
+        echo "x >= y: " . ($x >= $y) . "<br/>";
         ?>
     </body>
 </html>
